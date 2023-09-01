4569a598211d13cb5200c1d0714c6b6aecb782fb
PhongEarthTH <102400389+PhongEarthTH@users.noreply.github.com>
2023-09-01 21:58:09 +0700

    first commit

---
+++ lib/models/config.dart
@@ -0,0 +1,11 @@
+import 'package:crud/models/users.dart';
+
+class Configure {
+  static const server = "192.168.50.208";
+  static Users login = Users();
+  static List<String> gender = [
+    "None",
+    "Male",
+    "Female",
+  ];
+}
